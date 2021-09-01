ecbba9d2ad7c6cb644dcb04f55256bc2f2d04590
supporthawaii <kean.wong@supporthawaii.org>
2021-08-31 22:57:24 -1000

    Prep Import Offline Class Popup for refactoring to support Teachable courses

--- src/js/badging-administration.js
+++ src/js/badging-administration.js
@@ -1,1 +1,9 @@
-// All buttons linked to LIghtboxes and pages using WYSYWIG Wix editor
+// All other buttons linked to LIghtboxes and pages using WYSYWIG Wix editor
+
+import wixWindow from 'wix-window';
+
+$w.onReady(function () {
+    $w("#importOfflineClassBtn").onClick((event) => {
+        wixWindow.openLightbox("Badging Admin Import Offline Class Popup", {popupType: "offlineClass"});
+    });
+});
--- src/js/lightboxes/badging-admin-import-offline-class-popup.js
+++ src/js/lightboxes/badging-admin-import-offline-class-popup.js
@@ -1,62 +1,78 @@
 import wixData from 'wix-data';
+import wixWindow from 'wix-window';
 import {importOfflineClassCSV} from 'backend/badging-import-csv';
 
+const popupTypes = {
+    "offlineClass": {
+        "title": "IMPORT OFFLINE CLASS",
+        "importFunction": importOfflineClassCSV
+    }
+}
+
 $w.onReady(function () {
-    $w("#importPopupStatus").show();
-    $w("#importPopupStatus").text = "Fetching badge information...";
-    wixData.query("Badging-BadgesBrief")
-        .ascending("title")
-        .find()
-        .then( (results) => {
-            $w("#importPopupStatus").hide();
-            if (results.length > 0) {
-                $w("#badgeTypeDropdown").options = results.items.map((badge) => {
-                    return {"label":badge.title, "value":badge._id}
-                });
+    try {
+        let context = wixWindow.lightbox.getContext();
+        let popupType = popupTypes[context.popupType];
+
+        $w("#importPopupTitle").text = popupType.title;
+        $w("#importPopupStatus").show();
+        $w("#importPopupStatus").text = "Fetching badge information...";
+        wixData.query("Badging-BadgesBrief")
+            .ascending("title")
+            .find()
+            .then( (results) => {
+                $w("#importPopupStatus").hide();
+                if (results.length > 0) {
+                    $w("#badgeTypeDropdown").options = results.items.map((badge) => {
+                        return {"label":badge.title, "value":badge._id}
+                    });
+                } else {
+                    importError("NO BADGES FOUND");
+                }
+            });
+
+        $w("#uploadFileBtn").onChange(() => {
+            $w("#importPopupImportBtn").enable();
+        })
+
+        $w("#importPopupImportBtn").onClick(() => {
+            if ($w("#uploadFileBtn").value.length > 0) {
+                $w("#importPopupImportBtn").disable();
+                $w("#badgeTypeDropdown").disable();
+                $w("#uploadFileBtn").disable();
+                $w("#importPopupStatus").text = "Uploading file";
+                $w("#importPopupStatus").show();
+                $w("#uploadFileBtn").uploadFiles()
+                    .then( (uploadedFiles) => {
+                        uploadedFiles.forEach(uploadedFile => {
+                            $w("#importPopupStatus").text = "File uploaded, starting analysis";
+                            let badgeRef = $w("#badgeTypeDropdown").value;
+
+                            popupType.importFunction(uploadedFile.fileName, badgeRef)
+                                .then((response) => {
+                                    if (response.success === true) {
+                                        console.log("CSV imported successfully");
+                                        $w("#importPopupStatus").text = "CSV imported successfully.\nYou may close this popup";
+                                    }
+                                    else {
+                                        importError(response.errorMsg);
+                                    }
+                                })
+                                .catch( (importCSVError) => {
+                                    importError(importCSVError.message);
+                                });
+                        })
+                    })
+                    .catch( (importError) => {
+                        importError("File upload error " + importError.code + " : " + importError.message);
+                    } );
             } else {
-                importError("NO BADGES FOUND");
+                importError("No upload files found")
             }
         });
-
-    $w("#uploadFileBtn").onChange(() => {
-        $w("#importPopupImportBtn").enable();
-    })
-
-    $w("#importPopupImportBtn").onClick(() => {
-        if ($w("#uploadFileBtn").value.length > 0) {
-            $w("#importPopupImportBtn").disable();
-            $w("#badgeTypeDropdown").disable();
-            $w("#uploadFileBtn").disable();
-            $w("#importPopupStatus").text = "Uploading file";
-            $w("#importPopupStatus").show();
-            $w("#uploadFileBtn").uploadFiles()
-                .then( (uploadedFiles) => {
-                    uploadedFiles.forEach(uploadedFile => {
-                        $w("#importPopupStatus").text = "File uploaded, starting analysis";
-                        let badgeRef = $w("#badgeTypeDropdown").value;
-
-                        importOfflineClassCSV(uploadedFile.fileName, badgeRef)
-                            .then((response) => {
-                                if (response.success === true) {
-                                    console.log("CSV imported successfully");
-                                    $w("#importPopupStatus").text = "CSV imported successfully.\nYou may close this popup";
-                                }
-                                else {
-                                    importError(response.errorMsg);
-                                }
-                            })
-                            .catch( (importCSVError) => {
-                                importError(importCSVError.message);
-                            });
-                    })
-                })
-                .catch( (importError) => {
-                    importError("File upload error " + importError.code + " : " + importError.message);
-                } );
-        } else {
-            importError("No upload files found")
-        }
-    });
+    } catch (error) {
+        importError(error)
+    }
 });
 
 function importError(text) {
